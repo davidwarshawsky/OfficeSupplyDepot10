ebeae5265c43d1704ac464e971576c3def533e24
David Warshawsky <49329558+davidwarshawsky@users.noreply.github.com>
2023-03-08 16:43:33 -0800

    Add files via upload

---
+++ SignUpLogin.js
@@ -0,0 +1,10 @@
+// Get the sign up and login forms
+const signupForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
+
+// Add event listeners for form submissions
+signupForm.addEventListener("submit", handleSignupSubmit);
+loginForm.addEventListener("submit", handleLoginSubmit);
+
+// Function to handle sign
+
